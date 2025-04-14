7f9fe55dd5286557670cbd0198b1d07370a52a45
= <goutamagnihotri04@gmail.com>
2025-04-14 16:07:03 +0530

    integrate frontend with backend

---
+++ client/React/blogSiteClient/src/pages/LoginPage/Login.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import './Login'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import config from '../../../config';
+import { ToastContainer, toast } from 'react-toastify';
+
+function Login() {
+    console.log(config.url);
+    const [email , setEmail] = useState('');
+    const [password , setPassword] = useState('');
+    const navigate = useNavigate();
+    const  onLogin = async()=>{
+        console.log("Login function called");
+        console.log(email , password);
+        try{
+            const response = await axios.post(config.url+"/user/signin",{
+                email:email,
+                password:password
+            })
+            console.log(response);
+            if(response.data.status == 'success'){
+                toast.success("Signin Successfully");
+                navigate('/')
+            }else{
+                toast.warn("Invalid email or password");
+            }
+        } catch(e){
+            console.log("error occured while login :"+e);
+            toast("Something went wrong !!")
+
+        }
+       
+    }
+    return (
+        <div className="container">
+            <div >
+                <div className='heading'>Login</div>
+                <div className='body'>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td className="label"><label htmlFor="email">Email</label></td>
+                                <td className="input"><input type="email" id="email" onChange={(e)=>{setEmail(e.target.value)}}/></td>
+                            </tr>
+                            <tr>
+                                <td className="label"><label htmlFor="password">Password</label></td>
+                                <td className="input"><input type="password" id="password" onChange={(e)=>{setPassword(e.target.value)}}/></td>
+                            </tr>
+                            <tr>
+                                <td colSpan="2" className='buttons'>
+                                    <button className="button" onClick={onLogin}>Sign in</button>
+                                    <button className="button" onClick={()=>{
+                                        console.log("Sign up button clicked");
+                                        navigate('/register');
+                                    }}>Sign up</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ client/React/blogSiteClient/src/pages/RegisterPage/Register.jsx
@@ -0,0 +1,53 @@
+import React,{useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Register.css';
+
+function Registration() {
+    const navigate = useNavigate();
+    const [userObj,setUserInfo] = useState({
+        full_name:"Subham K",
+        email:"subham@gmail.com",
+        password:"1234",
+        phone_no:"4876327547"
+    })
+    const onRegister = ()=>{
+
+    }
+    return (
+        <div className='container'>
+            <div>
+                <div className='heading'>Register</div>
+                <div className='body'>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td className="label"><label htmlFor="email">Email</label></td>
+                                <td className="input"><input type="email" id="email" onChange={(e)=>setUserInfo({...userObj,email:e.target.value})}/></td>
+                            </tr>
+                            <tr>
+                                <td className="label"><label htmlFor="password">Password</label></td>
+                                <td className="input"><input type="password" id="password" /></td>
+                            </tr>
+                            <tr>
+                                <td className="label"><label htmlFor="fullname">Full Name</label></td>
+                                <td className="input"><input type="text" id="fullname" /></td>
+                            </tr>
+                            <tr>
+                                <td className="label"><label htmlFor="Phone">Phone No</label></td>
+                                <td className="input"><input type="tel" id="phone" /></td>
+                            </tr>
+                            <tr>
+                                <td colSpan="2" className='buttons'>
+                                    <button className="button">Sign up</button>
+                                    <button className="button" onClick={()=>{navigate('/')}}>Sign in</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Registration
